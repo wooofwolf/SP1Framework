b8376475c1c1dbabd3d8d2f3c6b93ff82755dd8a
Chua Kee Suan <chuakeesuan@gmail.com>
2020-08-19 11:44:16 +0800

    pull

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -246,8 +246,8 @@ void update(double dt)
 
 void splashScreenWait()    // waits for time to pass in splash screen
 {
-    //if (g_dElapsedTime > 3.0) // wait for 3 seconds to switch to game mode, else do nothing
-    //    g_eGameState = S_GAME;
+    if (g_dElapsedTime > 3.0) // wait for 3 seconds to switch to game mode, else do nothing
+        g_eGameState = S_GAME;
 }
 
 void updateGame()       // gameplay logic
