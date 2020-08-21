8270dfa2c4bf06bb9510322e56fe37a8d0a4ae0f
Zavier <200934Y@mymail.nyp.edu.sg>
2020-08-21 15:49:12 +0800

    Waterboy and fire boy Projectile collision with NPC

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -413,36 +413,40 @@ void charAbility()
         // Water boy Shooting
         else if (rOrC == 0)
         {
-            if (lastMove2 == 1 && Collision(g_sPjtl2.m_cLocation, 'U') == false)
-            {
-                g_sPjtl2.m_cLocation.Y -= 1;
-                if (g_sPjtl2.m_cLocation.Y == g_sChar.m_cLocation.Y)
+            for (int n = 0; n < 10; n++) {
+                if (lastMove2 == 1 && Collision(g_sPjtl2.m_cLocation, 'U') == false)
                 {
-                    g_sPjtl2.m_cLocation = g_sChar2.m_cLocation;
+                    g_sPjtl2.m_cLocation.Y -= 1;
+                    if (g_sPjtl2.m_cLocation.Y == g_sChar.m_cLocation.Y || g_sPjtl2.m_cLocation.Y == static_cast<npc*>(npcPtr[n])->getCoords().Y)
+                    {
+                        g_sPjtl2.m_cLocation = g_sChar2.m_cLocation;
+                    }
                 }
-            }
-            else if (lastMove2 == 2 && Collision(g_sPjtl2.m_cLocation, 'L') == false)
-            {
-                g_sPjtl2.m_cLocation.X -= 1;
-                if (g_sPjtl2.m_cLocation.X == g_sChar.m_cLocation.X)
+                else if (lastMove2 == 2 && Collision(g_sPjtl2.m_cLocation, 'L') == false)
                 {
-                    g_sPjtl2.m_cLocation = g_sChar2.m_cLocation;
+                    g_sPjtl2.m_cLocation.X -= 1;
+                    if (g_sPjtl2.m_cLocation.X == g_sChar.m_cLocation.X || g_sPjtl2.m_cLocation.X == static_cast<npc*>(npcPtr[n])->getCoords().X)
+                    {
+                        g_sPjtl2.m_cLocation = g_sChar2.m_cLocation;
+                    }
                 }
-            }
-            else if (lastMove2 == 3 && Collision(g_sPjtl2.m_cLocation, 'D') == false)
-            {
-                g_sPjtl2.m_cLocation.Y += 1;
-                if (g_sPjtl2.m_cLocation.Y == g_sChar.m_cLocation.Y)
+                else if (lastMove2 == 3 && Collision(g_sPjtl2.m_cLocation, 'D') == false)
                 {
-                    g_sPjtl2.m_cLocation = g_sChar2.m_cLocation;
+                    g_sPjtl2.m_cLocation.Y += 1;
+                    if (g_sPjtl2.m_cLocation.Y == g_sChar.m_cLocation.Y || g_sPjtl2.m_cLocation.Y == static_cast<npc*>(npcPtr[n])->getCoords().Y)
+                    {
+                        g_sPjtl2.m_cLocation = g_sChar2.m_cLocation;
+                    }
                 }
-            }
-            else if (lastMove2 == 4 && Collision(g_sPjtl2.m_cLocation, 'R') == false)
-            {
-                g_sPjtl2.m_cLocation.X += 1;
-                if (g_sPjtl2.m_cLocation.X == g_sChar.m_cLocation.X)
+                else if (lastMove2 == 4 && Collision(g_sPjtl2.m_cLocation, 'R') == false)
                 {
-                    g_sPjtl2.m_cLocation = g_sChar2.m_cLocation;
+                    g_sPjtl2.m_cLocation.X += 1;
+
+                    if (g_sPjtl2.m_cLocation.X == g_sChar.m_cLocation.X || g_sPjtl2.m_cLocation.X == static_cast<npc*>(npcPtr[n])->getCoords().X)
+                    {
+                        g_sPjtl2.m_cLocation = g_sChar2.m_cLocation;
+                    }
+
                 }
             }
         }
