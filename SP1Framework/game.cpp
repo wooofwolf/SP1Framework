e67050f2c7144077d65a8ebaa1e668f492ba3661
TAI YANG TAT BRYSON <202680K@mymail.nyp.edu.sg>
2020-08-25 11:24:02 +0800

    Added Map Selection Screen

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -992,7 +992,7 @@ void renderMap()
     // Settings
     else if (mapNum == 2 && mapSel == true)
     {
-        
+        g_Console.writeToBuffer(3, 3, "Map Selection: ", 0xB0);
     }
 
     // Tutorial
